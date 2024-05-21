4e2d7348488d152ea13baa6c824eee7671c5f8d5
Enzo Grigorio <enzonascentess@gmail.com>
2024-05-21 10:18:25 +0100

    adicionei comentários

--- front/front-egest/src/funcionarios/Index.jsx
+++ front/front-egest/src/funcionarios/Index.jsx
@@ -8,6 +8,8 @@ const Funcionarios = () => {
         switch(acao) {
             case 'adicionar': return <Form />;
             case 'editar': 
+                /* a ideia aqui é ser uma tabela, ao clicar no lapis de edição, adicionar o editar nos parametros junto com o id do utilizador
+                   a que se deseja atualizar, pra depois aqui embaixo pegar o id dele e montar o formulário de edição */
                 const id = params.get('id');
                 return <Form id={id} />; 
             case 'remover': return <h1>Remover Funcionário</h1>;            
