6225e3ee5c7191c1dd4bc3bec0aa2eb5fe3f616c
lfscastanheira <135338478+lfscastanheira@users.noreply.github.com>
2024-05-26 19:14:11 +0100

    Login Novo
    final sate

--- front/front-egest/src/auth/Login.jsx
+++ front/front-egest/src/auth/Login.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import api from '../api/api';
 import Swal from 'sweetalert2';
 
-const handleSubmit = async (e) => {
+const Login = () => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const username = e.target.username.value;
     const password = e.target.password.value;
@@ -13,75 +14,47 @@ const handleSubmit = async (e) => {
     } catch (error) {
       if (error.response.status === 404) Swal.fire('Erro', 'Usuário não encontrado', 'error');
     }
-};
+  };
 
-const Login = () => {
-    return (
-      <body className="bg-light">
-        <section class="p-3 p-md-4 p-xl-5">
-          <div class="container mb-5 mt-5">
-            <div class="row justify-content-center">
-              <div class="col-12 col-xxl-11">
-                <div class="card border-light-subtle shadow-sm">
-                  <div class="row g-0">
-                    <div class="col-12 col-md-6">
-                      <img class="img-fluid rounded-image w-100 h-100 object-fit-cover" loading="lazy" src="./contents/styling/DECOR_1.jpg" alt="Welcome back you've been missed!" id="loginIMG" />
-                    </div>
-                    <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                      <div class="col-12 col-lg-11 col-xl-10">
-                        <div class="card-body p-3 p-md-4 p-xl-5">
-                          <div class="row">
-                            <div class="col-12">
-                              <div class="mb-5">
-                                <div class="text-center mb-4">
-                                  <a href="#!">
-                                    <img src="./contents/branding/2dLogo.png" alt="Songbird Logo" width="175" height="57"></img>
-                                  </a>
-                                </div>
-                                <h4 class="text-center">Bem vindo de volta!</h4>
-                              </div>
-                            </div>
-                          </div>
-                          <form onSubmit={handleSubmit}>
-                            <div class="row gy-3 overflow-hidden">
-                              <div class="col-12">
-                                <div class="form-floating mb-3">
-                                  <input type="text" class="form-control" name="username" id="username" placeholder="username" required/>
-                                  <label for="username" class="form-label">Username</label>
-                                </div>
-                              </div>
-                              <div class="col-12">
-                                <div class="form-floating mb-3">
-                                  <input type="password" class="form-control" name="password" id="password" placeholder="password" required/>
-                                  <label for="password" class="form-label">Password</label>
-                                </div>
-                              </div>
-                              <div class="col-12">
-                                <div class="d-grid">
-                                  <button class="btn btn-dark btn-lg" type="submit" value="login">Login</button>
-                                </div>
-                              </div>
-                            </div>
-                          </form>
-                          <div class="row">
-                            <div class="col-12">
-                              <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-center mt-3">
-                                <a href="register.php" class="link-secondary text-decoration-none">Criar Conta</a>
-                                <a href="#!" class="link-secondary text-decoration-none">Esqueceu a Password?</a>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+  return (
+    <div className="light min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
+        <h2 className="text-2xl text-gray-800 text-center font-bold">egest</h2>
+        <p className="mt-2 text-center text-gray-600">entra na tua conta</p>
+        <form className="mt-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col mb-4">
+            <label className="text-gray-800 font-semibold" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="px-3 py-2 bg-gray-200 text-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              id="username"
+              name="username"
+              placeholder="insira o seu username"
+              required
+              type="text"
+            />
           </div>
-        </section>
-      </body>
-    );
+          <div className="flex flex-col mb-6">
+            <label className="text-gray-800 font-semibold" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="px-3 py-2 bg-gray-200 text-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              id="password"
+              name="password"
+              placeholder="insira a sua password"
+              required
+              type="password"
+            />
+          </div>
+          <button className="w-full py-2 px-4 bg-indigo-500 text-white rounded-md hover:bg-indigo-600" type="submit" value="login">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
